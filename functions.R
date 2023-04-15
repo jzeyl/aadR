1349767ecbdc2e3d7443eda1d48279ea657c0be8
= <jeffzeyl@gmail.com>
2023-04-15 12:23:28 -0400

    testing functions and small code cleaning

--- functions.R
+++ functions.R
@@ -3,7 +3,8 @@ browse_audiograms<-function(){
   url<- "https://www.animalaudiograms.org/api/v1/browse"
   res <- GET("https://www.animalaudiograms.org/api/v1/browse")
   dat <- fromJSON(rawToChar(res$content))
-  assign("browse",dat, envir = .GlobalEnv)
+  #return(dat)
+  assign("aad_audiograms",dat, envir = .GlobalEnv)
 }
 
 #GET AUDIOGRAM DATA FOR A GIVEN SPECIES
@@ -12,9 +13,9 @@ get_species<-function(name){
   browse_audiograms()
 
   #find id numbers for that species
-  idlist<-browse$id[browse$species_name==name]
+  idlist<-aad_audiograms$id[aad_audiograms$species_name==name]
 
-  #get audiogram for that as a list
+  #get audiogram for that species as a list
   audiogramlst<-list()
   for (i in idlist) {
     # ... make some data
@@ -26,10 +27,12 @@ get_species<-function(name){
     dat$species<-as.character(name)
     audiogramlst[[i]] <- dat # add it to your list
   }
+  
   #convert list of single audiograms to a dataframe
-
   big_data <- do.call(rbind, audiogramlst)
-  assign(paste0("audiograms_",name),big_data,envir = .GlobalEnv)
+  withsplunits<-left_join(big_data,SPL_units, by = c("sound_pressure_level_reference_id" = "id"))
+  assign(paste0("audiograms_",name),withsplunits,envir = .GlobalEnv)
+  
 }
 
 #SHOW WHICH SPECIES ARE AVAILABLE IN THE DATABASE
@@ -44,5 +47,7 @@ species_available<-function(common = FALSE){
   }
 }
 
-
+#this object converts the SPL reference ID to the actual units. It is joined to the when you grab the species to include the actual reference value and units
 SPL_units <- read_json("SPL units.json", simplifyVector = TRUE)
+#drop unused  columns
+SPL_units <-SPL_units %>% select("id", "spl_reference_value","spl_reference_unit")
